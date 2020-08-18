0805cb61bed3961da21716e8b307c0872015eb91
Devidev <47260520+dev634@users.noreply.github.com>
2020-08-18 15:19:15 +0200

    touch screen setting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
 const images = document.querySelectorAll(".img");
 const texts = document.querySelectorAll(".text");
-let alreadyOpen = null;
+const container = document.querySelector(".container");
+let move = 0;
 
-function openTrans(e) {}
+function handleTouchMove(e) {
+	if (e.type == "touchstart") {
+		e.preventDefault();
+	} else if (e.type == "touchmove") {
+		e.preventDefault();
+	} else if (e.type == "touchend") {
+		e.target.parentNode.style.position = "relative";
+		move += -100;
+		if (move == -500) {
+			move = 0;
+		}
+		e.target.parentNode.style.left = move + "%";
+	}
+}
 
 function closing(e) {
 	this.classList.remove("is-open-active", "is-open");
@@ -21,7 +35,14 @@ function opening(e) {
 	this.addEventListener("transitionend", myTransit);
 }
 
-images.forEach(function (element) {
-	element.addEventListener("click", opening);
-	element.addEventListener("mouseout", closing);
-});
+if (window.innerHeight > window.innerWidth) {
+	container.addEventListener("touchstart", handleTouchMove);
+	container.addEventListener("touchmove", handleTouchMove);
+	container.addEventListener("touchend", handleTouchMove);
+} else {
+	images.forEach(function (element) {
+		element.addEventListener("click", opening);
+		element.addEventListener("touchstart", opening);
+		element.addEventListener("mouseleave", closing);
+	});
+}
